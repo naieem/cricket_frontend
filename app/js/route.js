3629784b31aab7a929d14895d3f1876819b4fa16
naieemsupto <naieem.mahmud@selise.ch>
2017-02-22 19:09:21 +0600

    route code is now moved to json file

--- app/js/route.js
+++ app/js/route.js
@@ -1,10 +1,12 @@
 (function() {
     'use strict';
+    var stateprovider = null;
     angular
         .module('cricket')
         .config(config);
 
-    function config($stateProvider, $urlRouterProvider, $httpProvider) {
+    function config($stateProvider, $urlRouterProvider) {
+        stateprovider = $stateProvider;
         $urlRouterProvider.otherwise('/');
 
         $stateProvider
@@ -15,87 +17,105 @@
             templateUrl: 'template/home.html',
             controller: 'homeController',
             controllerAs: 'home'
-        })
+        });
 
-        .state('toss', {
-            url: '/toss',
-            templateUrl: 'template/toss.html',
-            controller: 'tossController',
-            controllerAs: 'toss'
-        })
-
-        .state('play', {
-            url: '/play',
-            templateUrl: 'template/play.html',
-            controller: 'playController',
-            controllerAs: 'play'
-        })
-
-        // nested list with custom controller
-        // .state('home.list', {
-        //     url: '/list',
-        //     templateUrl: 'template/partial-home-list.html',
-        //     controller: function($scope) {
-        //         $scope.dogs = ['Bernese', 'Husky', 'Goldendoodle'];
-        //     }
+        // .state('toss', {
+        //     url: '/toss',
+        //     templateUrl: 'template/toss.html',
+        //     controller: 'tossController',
+        //     controllerAs: 'toss'
         // })
 
-        // // nested list with just some random string data
-        // .state('home.paragraph', {
-        //     url: '/paragraph',
-        //     template: 'I could sure use a drink right now.'
+        // .state('play', {
+        //     url: '/play',
+        //     templateUrl: 'template/play.html',
+        //     controller: 'playController',
+        //     controllerAs: 'play'
         // })
 
-        // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
-        // .state('about', {
-        //     url: '/about',
+        // // nested list with custom controller
+        // // .state('home.list', {
+        // //     url: '/list',
+        // //     templateUrl: 'template/partial-home-list.html',
+        // //     controller: function($scope) {
+        // //         $scope.dogs = ['Bernese', 'Husky', 'Goldendoodle'];
+        // //     }
+        // // })
+
+        // // // nested list with just some random string data
+        // // .state('home.paragraph', {
+        // //     url: '/paragraph',
+        // //     template: 'I could sure use a drink right now.'
+        // // })
+
+        // // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
+        // // .state('about', {
+        // //     url: '/about',
+        // //     templateUrl: 'template/matches.html',
+        // //     // views: {
+        // //     //     '': { templateUrl: 'template/partial-about.html' },
+        // //     //     'columnOne@about': { template: 'Look I am a column!' },
+        // //     //     'columnTwo@about': {
+        // //     //         templateUrl: 'template/table-data.html',
+        // //     //         controller: 'scotchController'
+        // //     //     }
+        // //     // }
+        // //     controller: function() {
+        // //         this.title = 'My Contacts';
+        // //     },
+        // //     controllerAs: 'contact'
+
+        // // });
+
+        // .state('matches', {
+        //     url: '/matches',
         //     templateUrl: 'template/matches.html',
-        //     // views: {
-        //     //     '': { templateUrl: 'template/partial-about.html' },
-        //     //     'columnOne@about': { template: 'Look I am a column!' },
-        //     //     'columnTwo@about': {
-        //     //         templateUrl: 'template/table-data.html',
-        //     //         controller: 'scotchController'
+        //     controller: 'allMatchesController',
+        //     controllerAs: 'matches'
+        // })
+
+        // .state('details', {
+        //     url: '/matches/details/:matchId',
+        //     templateUrl: 'template/matchDetails.html',
+        //     controller: 'matchDetails',
+        //     controllerAs: 'details',
+        //     // resolve: {
+        //     //     user: function($stateParams,$http) {
+        //     //         $http.get("http://localhost:3000/crud/details/" + $stateParams.matchId)
+        //     //             .then(function(response) {
+        //     //                 return response.data;
+        //     //                 // if (vm.matchDetails.totalRun === 0) {
+        //     //                 //     vm.hideDetails = true;
+        //     //                 //     vm.message = "Match Abandoned Without a ball being bowled";
+        //     //                 // }
+        //     //             });
         //     //     }
         //     // }
-        //     controller: function() {
-        //         this.title = 'My Contacts';
-        //     },
-        //     controllerAs: 'contact'
+        // })
 
+        // .state('detailsByOverBall', {
+        //     url: '/matches/details/:matchId/:over/:ball',
+        //     templateUrl: 'template/matchDetailsByOverBall.html',
+        //     controller: 'matchDetailsByOverBall',
+        //     controllerAs: 'details'
         // });
-
-        .state('matches', {
-            url: '/matches',
-            templateUrl: 'template/matches.html',
-            controller: 'allMatchesController',
-            controllerAs: 'matches'
-        })
-
-        .state('details', {
-            url: '/matches/details/:matchId',
-            templateUrl: 'template/matchDetails.html',
-            controller: 'matchDetails',
-            controllerAs: 'details',
-            // resolve: {
-            //     user: function($stateParams,$http) {
-            //         $http.get("http://localhost:3000/crud/details/" + $stateParams.matchId)
-            //             .then(function(response) {
-            //                 return response.data;
-            //                 // if (vm.matchDetails.totalRun === 0) {
-            //                 //     vm.hideDetails = true;
-            //                 //     vm.message = "Match Abandoned Without a ball being bowled";
-            //                 // }
-            //             });
-            //     }
-            // }
-        })
-
-        .state('detailsByOverBall', {
-            url: '/matches/details/:matchId/:over/:ball',
-            templateUrl: 'template/matchDetailsByOverBall.html',
-            controller: 'matchDetailsByOverBall',
-            controllerAs: 'details'
-        });
     }
+
+    angular
+        .module('cricket').run(["$http", function($http) {
+            $http.get('../route.json').then(function(response) {
+                console.log(response);
+                angular.forEach(response.data, function(value, key) {
+                    console.log(value.controller);
+                    var state = {
+                        "url": value.url,
+                        "templateUrl": value.templateUrl,
+                        "controller": value.controller,
+                        "controllerAs": value.controllerAs,
+                    };
+                    stateprovider.state(value.name, state);
+                });
+            });
+
+        }]);
 })();
